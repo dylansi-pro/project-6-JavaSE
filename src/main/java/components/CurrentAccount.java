7c3439f0b5279286c94c32f3c7559e0417889a72
dylansi-pro <dylansi.hi@gmail.com>
2026-07-08 16:02:13 +0200

    1.2.2 Creation of the CurrentAccount and SavingsAccount

--- src/main/java/components/CurrentAccount.java
+++ src/main/java/components/CurrentAccount.java
@@ -1,5 +1,6 @@
 package components;
 
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
 public class CurrentAccount extends Account {
     public CurrentAccount(String label, double balance, Client client) {
         super(label, balance, client);
--- src/main/java/components/SavingsAccount.java
+++ src/main/java/components/SavingsAccount.java
@@ -1,5 +1,6 @@
 package components;
 
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
 public class SavingsAccount extends Account {
     public SavingsAccount(String label, double balance, Client client) {
         super(label, balance, client);
