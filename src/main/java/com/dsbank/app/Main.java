36e1bd03a03acc1cc4bb147499e129556e632063
dylansi-pro <dylansi.hi@gmail.com>
2026-07-08 15:03:02 +0200

    Creation of the client class

--- src/main/java/com/dsbank/app/Main.java
+++ src/main/java/com/dsbank/app/Main.java
@@ -1,5 +1,9 @@
+package com.dsbank.app;
+
+import components.Client;
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/main/java/components/Client.java
@@ -0,0 +1,34 @@
+package components;
+
+// 1.1.1 Creation of the client class
+public class Client {
+    private String name;
+    private String firstName;
+    private static int counter = 0;
+    private int clientNumber;
+
+    public Client(String name,String firstName ,int clientNumber) {
+        this.name = name;
+        this.firstName = firstName;
+        this.clientNumber = counter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getClientNumber() {
+        return clientNumber;
+    }
+
+}
